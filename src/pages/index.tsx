734ea6f113599790abbde0b85d0f24c34675d16c
Shawn Rivers <kaushou@gmail.com>
2021-04-24 13:07:51 +0900

    feat: add index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+  return (
+    <div className="flex flex-col text-center font-mono">
+      <h1 className="text-4xl font-bold mt-8">GIF Creator</h1>
+    </div>
+  );
+};
+
+export default Home;
