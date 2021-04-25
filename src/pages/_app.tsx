c3114ccd9c13251b557fa2dba4ce021a91526234
Shawn Rivers <kaushou@gmail.com>
2021-04-25 13:48:45 +0900

    feat: add app meta data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,37 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <main className="font-mono text-gray-900 px-4 py-8">
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>To GIF</title>
+        <meta name="title" content="To GIF" />
+        <meta
+          name="description"
+          content="A web app that generates GIF from video file using FFmpeg WASM"
+        />
+        <meta name="url" content="togif.vercel.app" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name="og:title" content="To GIF" />
+        <meta
+          name="og:description"
+          content="A web app that generates GIF from video file using FFmpeg WASM"
+        />
+        <meta name="og:type" content="website" />
+        <meta name="og:url" content="togif.vercel.app" />
+        <meta name="og:image:alt" content="To GIF" />
+        <meta name="twitter:creator" content="@yuxiao_he" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      </Head>
+      <main className="font-mono text-gray-900 px-4 py-8">
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 };
 
