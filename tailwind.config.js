eb99a363069438062c26b3657d51de33b11fbfa0
Shawn Rivers <kaushou@gmail.com>
2021-04-26 21:12:07 +0900

    build: enable Tailwind JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {
-      outline: ['focus-visible'],
-      ringColor: ['focus-visible'],
-      ringOpacity: ['focus-visible'],
-      ringWidth: ['focus-visible'],
-      ringOffsetWidth: ['focus-visible'],
-      opacity: ['disabled'],
-      cursor: ['hover'],
-    },
-  },
+  // variants: {
+  //   extend: {
+  //     outline: ['focus-visible'],
+  //     ringColor: ['focus-visible'],
+  //     ringOpacity: ['focus-visible'],
+  //     ringWidth: ['focus-visible'],
+  //     ringOffsetWidth: ['focus-visible'],
+  //     opacity: ['disabled'],
+  //     cursor: ['hover'],
+  //   },
+  // },
   plugins: [],
 };
