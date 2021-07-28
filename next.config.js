c61ff3f4e508250b8d219ae0c60163529a316b05
Shawn Rivers <kaushou@gmail.com>
2021-07-28 21:56:50 +0900

    fix: add cross-origin headers to fix SharedArrayBuffer not defined issues in Chrome

--- next.config.js
+++ next.config.js
@@ -2,4 +2,24 @@ module.exports = {
   future: {
     webpack5: true,
   },
+  async headers() {
+    // Add cross-origin headers because SharedArrayBuffer is 
+    // limited to cross-origin isolated pages after Chrome 92.
+    // https://github.com/ffmpegwasm/ffmpeg.wasm/issues/231
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
 };
